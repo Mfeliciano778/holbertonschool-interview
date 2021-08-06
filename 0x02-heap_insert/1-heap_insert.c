e1491a09db9115e7fa01d45ac6ef604cff5fd325
Mfeliciano778 <1652@holbertonschool.com>
2021-08-06 02:18:14 +0000

    Betty compliant

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -12,37 +12,39 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    binary_tree_t *new_node;
-    // Check if the root and *root is empty and if so create new node as 
-    // head node
-    if (!(*root))
-    {
-        new_node = binary_tree_node(NULL, value);
-        (*root) = new_node;
-        return (new_node);
-    }
-    else
-    {
-        if ((*root)->n < value)
-        {
-            // create new node
-            new_node = binary_tree_node((*root), value);
-            (*root)->left = new_node;
-            // swap the values
-            swap((*root), new_node);
-        }
+	binary_tree_t *new_node;
+	/**
+	 * // Check if the root and *root is empty and if so create new node as
+	 * // head node
+	*/
+	if (!(*root))
+	{
+		new_node = binary_tree_node(NULL, value);
+		(*root) = new_node;
+		return (new_node);
+	}
+	else
+	{
+		if ((*root)->n < value)
+		{
+			new_node = binary_tree_node((*root), value);
+			(*root)->left = new_node;
+			swap((*root), new_node);
+		}
 
-    }
-    // Assuming it's not empty check if the value is greater than root
-    // we are checking, if so swap nodes of root and new node where root now
-    //  
-    // becomes the value
+	}
+	/*
+	 * // Assuming it's not empty check if the value is greater than root
+	 * // we are checking, if so swap nodes of root and new node where root now
+	 * //
+	 * // becomes the value
 
-    // check child nodes exists against the value of the root node and swap
-    // first left then right, then repeat if necessary
-    
+	 * // check child nodes exists against the value of the root node and swap
+	 * // first left then right, then repeat if necessary
+	*/
 
-    return (new_node);
+
+	return (new_node);
 }
 /**
  * swap - swaps the pointers that are given
@@ -54,8 +56,8 @@ heap_t *heap_insert(heap_t **root, int value)
  */
 void swap(heap_t *a, heap_t *b)
 {
-    int temp = a->n;
+	int temp = a->n;
 
-    a->n = b->n;
-    b->n = temp;
+	a->n = b->n;
+	b->n = temp;
 }
