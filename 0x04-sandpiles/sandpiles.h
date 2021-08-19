002467c5faa0e1e0d06ff5e83c4eea65fee45fd2
Mfeliciano778 <1652@holbertonschool.com>
2021-08-19 17:58:42 +0000

    Made the header file betty compliant

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,8 +2,6 @@
 #define SANDPILES_H
 #include <stdio.h>
 #include <stdlib.h>
-// static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-// static void print_grid(int grid[3][3]);
 int main(void);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void topple_sand(int grid1[3][3]);
