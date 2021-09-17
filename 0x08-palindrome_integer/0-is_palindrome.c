6f77698b01f9b27f7fa78ca78f2a0ce838336c76
Mfeliciano778 <1652@holbertonschool.com>
2021-09-16 20:04:44 -0400

    Made Betty compliant

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -8,19 +8,19 @@
  *
  * Return: 1 if n is a palindrome, and 0 otherwise
  */
- int is_palindrome(unsigned long n)
- {
-    int last_digit, n_copy = n, reverse = 0;
+int is_palindrome(unsigned long n)
+{
+	int last_digit, n_copy = n, reverse = 0;
 
-    if (n < 10)
-	    return (1);
-    while (n != 0)
-    {
-        last_digit = n % 10;
-        reverse = reverse * 10 + last_digit;
-        n = n / 10;
-    }
-    if (n_copy == reverse)
-        return (1);
-    return (0);
- }
+	if (n < 10)
+		return (1);
+	while (n != 0)
+	{
+		last_digit = n % 10;
+		reverse = reverse * 10 + last_digit;
+		n = n / 10;
+	}
+	if (n_copy == reverse)
+		return (1);
+	return (0);
+}
