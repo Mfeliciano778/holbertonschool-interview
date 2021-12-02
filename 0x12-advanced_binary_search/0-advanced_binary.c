380c5ef25108e331f4a81172683efdfbf80b662e
Mfeliciano778 <mattfeliciano778@gmail.com>
2021-12-02 15:23:08 -0500

    Initial commit of task 0

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in sorted array of integers
+ * @array: array
+ * @size: array size
+ * @value: value
+ * Return: index at value, else -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (simple_binary(array, 0, size - 1, value));
+}
+/**
+ * simple_binary - searches through an array like a binary tree
+ * @array: array
+ * @left: left index
+ * @right: right index
+ * @value: value
+ * Return: index at value, else -1
+ */
+int simple_binary(int *array, int left, int right, int value)
+{
+	int mid;
+
+	if (right >= left)
+	{
+		print_array(array, left, right);
+
+		mid = left + (right - left) / 2;
+		if (array[mid] == value && array[mid - 1] != value)
+			return (mid);
+		if (array[mid] >= value)
+			return (simple_binary(array, left, mid, value));
+		return (simple_binary(array, mid + 1, right, value));
+	}
+	return (-1);
+}
+/**
+* print_array - prints the array
+* Return: void
+* @array: pointer to first element
+* @start: where to start printing
+* @size: how much to print
+*/
+void print_array(int *array, int start, int size)
+{
+	printf("Searching in array: %i", array[start]);
+	for (; start <= size - 1; start++)
+		printf(", %i", array[start]);
+	printf("\n");
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int simple_binary(int *array, int left, int right, int value);
+void print_array(int *array, int start, int size);
+
+#endif
