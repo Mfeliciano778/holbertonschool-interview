d364f2ba43ceef7160b0c02d8d482978d0b9f929
Mfeliciano778 <1652@holbertonschool.com>
2022-03-10 22:13:08 -0500

    Made Betty compliant

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings
+ * wildcmp - Compares two strings and one can contain an astrix
  * @s1: First string
  * @s2: Second string
  * Returns: 1 if two strings are identical, 0 if not.
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == 0 && *s2 == 0)
 		return (1);
 
-    if (*s1 == *s2)
+	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
